ef176c5d433d809590943540fa70e221ca633a66
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-10-31 04:24:20 +0200

    add 8-concat_edges.py file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = str[:5]
+str1 = str[:6]
 str2 = str[39:66]
 str3 = str[107:111]
 str = str2 + " " + str3 + " " + str1
